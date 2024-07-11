132c335662005964469c6bf190fbd368113919b5
Kenny Chung <kennychung.techsolution@gmail.com>
2024-07-11 12:18:00 +0800

    Implemented request handling functions for Servy.Hanlder module

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -3,34 +3,80 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> route
     |> format_response
   end
 
+  def log(conv), do: IO.inspect conv
+
   def parse(request) do
-    #TODO Parse the request sting into a map.
-    conv = %{ method: "GET", path: "/wildthings", resp_body: ""}
+    [ method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first
+      |> String.split(" ")
+
+    %{  method: method,
+        path: path,
+        resp_body: "",
+        status: nil
+    }
+
   end
 
   def route(conv) do
-    conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears"}
+    route(conv, conv.method, conv.path)
+  end
+
+  def route(conv, "GET", "/wildthings") do
+    %{ conv| status: 200, resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
+  end
+
+  def route(conv, "GET", "/bears/"<> id) do
+    %{ conv | status: 200, resp_body: "Bear #{id}"}
+  end
+
+  def route(conv, "DELETE", "/bears/"<> id) do
+    %{ conv | status: 403, resp_body: "Deleting a bear is forbidden"}
+  end
+
+  def route(conv, _method, path) do
+    %{ conv | status: 404, resp_body: "No #{path} here"}
   end
 
   def format_response(conv) do
     """
-      HTTP/1.1 200 OK\r\n
-      Content-Type: text/html;
-      Content-Length: 20
+      HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+      Content-Type: text/html
+      Content-Length: #{byte_size(conv.resp_body)}
 
-      Bears, Lions, Tigers
+      #{conv.resp_body}
     """
   end
 
-  request = """
-    GET /wildthings HTTP/1.0\r\n
-    Host: example.com\r\n
-    User-Agent: ExampleBrowser/1.0\r\n
-    Accept: */*\r\n
 
-  """
+  defp status_reason(code) do
+    %{
+      200=> "OK",
+      201=> "Created",
+      401=> "Unauthorized",
+      403=> "Forbidden",
+      404=> "Not Found",
+      500=> "Internal Server Error"
+    }[code]
+  end
 end
+
+request = """
+DELETE /bears/2 HTTP/1.0
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+"""
+
+Servy.Handler.handle(request)
