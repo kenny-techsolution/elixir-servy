b8a6cc0986ef90c22d154787818b1634bd4b3dfc
Kenny Chung <kennychung.techsolution@gmail.com>
2024-07-11 14:47:38 +0800

    Refactored and added moduledoc

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,5 +1,9 @@
 defmodule Servy.Handler do
+  require Logger
 
+  @moduledoc "Handles HTTP requests"
+
+  @doc "transform the request into a response"
   def handle(request) do
     request
     |> parse
@@ -38,11 +42,11 @@ defmodule Servy.Handler do
     %{conv | path: "/wildthings" }
   end
 
-  def rewrite_path(conv), do: conv
+  def rewrite_path(%{path: "/bears?id=" <> id}=conv) do
+    %{conv | path: "/bears/#{id}" }
+  end
 
-  # def route(conv) do
-  #   route(conv, conv.method, conv.path)
-  # end
+  def rewrite_path(conv), do: conv
 
   def route(%{ method: "GET", path: "/wildthings" } = conv) do
     %{ conv| status: 200, resp_body: "Bears, Lions, Tigers"}
@@ -52,19 +56,59 @@ defmodule Servy.Handler do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
+  @pages_path Path.expand("../../pages", __DIR__)
+
+  def route(%{ method: "GET", path: "/bears/new" } = conv) do
+    @pages_path
+    |> Path.join("form.html")
+    |> File.read
+    |> handle_file(conv)
+  end
+
   def route(%{ method: "GET", path: "/bears/"<>id } = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}"}
   end
 
-  def route(%{ method: "DELETE", path: "/bears/" <> id} =conv) do
-    %{ conv | status: 403, resp_body: "Deleting a bear #{id} is forbidden"}
+  def route(%{ method: "DELETE", path: "/bears/" <> _id} =conv) do
+    %{ conv | status: 403, resp_body: "Deleting a bear is forbidden"}
+  end
+
+  # def route(%{ method: "GET", path: "/about"}=conv) do
+  #     Path.expand("../../pages", __DIR__)
+  #     |> Path.join("about.html")
+  #     |> File.read
+  #     |> handle_file(conv)
+  # end
+
+  def route(%{ method: "GET", path: "/" <> name}=conv) do
+    @pages_path
+    |> Path.join("#{name}.html")
+    |> File.read
+    |> handle_file(conv)
   end
 
   def route(%{ path: path } = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here"}
   end
 
+  def handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content}
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{ conv | status: 404, resp_body: "File not found" }
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{ conv | status: 500, resp_body: "File error:#{reason}"}
+  end
+
+  def emojify(%{ status: 200}=conv) do
+    %{ conv | resp_body: "🐻 #{conv.resp_body} 🐻"}
+  end
+
   def format_response(conv) do
+    Logger.warn "format_response is called"
     """
       HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
       Content-Type: text/html
@@ -88,7 +132,7 @@ defmodule Servy.Handler do
 end
 
 request = """
-DELETE /bears/2 HTTP/1.0
+GET /bears/new HTTP/1.0
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
